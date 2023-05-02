0cc8bdb1688e0e07cdbb9aacf2676c25937cb591
Gye-jin <109066658+Gye-jin@users.noreply.github.com>
2023-05-02 16:28:10 +0900

    ComponentScan에 대한 이해

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",   -> Defalult: @ComponentScan이 붙은 곳(hello.core 패키지)
+//        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
 )
 public class AutoAppConfig {
